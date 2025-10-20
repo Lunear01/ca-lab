05ea235e01891d4a0f12bed86d49ec0d4d237560
Lunear01 <IkerHuang0315@gmail.com>
2025-10-20 13:37:13 -0400

    LogoutController implemented

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,5 +1,6 @@
 package interface_adapter.logout;
 
+import use_case.login.LoginInputData;
 import use_case.logout.LogoutInputBoundary;
 
 /**
@@ -7,16 +8,17 @@ import use_case.logout.LogoutInputBoundary;
  */
 public class LogoutController {
 
-    private LogoutInputBoundary logoutUseCaseInteractor;
+    private final LogoutInputBoundary logoutUseCaseInteractor;
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
-        // TODO: Save the interactor in the instance variable.
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
 
     /**
      * Executes the Logout Use Case.
      */
     public void execute() {
-        // TODO: run the use case interactor for the logout use case
+        logoutUseCaseInteractor.execute();
+
     }
 }
